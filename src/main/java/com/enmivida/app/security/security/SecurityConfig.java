d7214b13e912e366ba470f46093aaf4c8dd5217d
ingfrf <ingfrf@gmail.com>
2024-01-17 00:02:31 +0100

    master users in memory + msyql connection

--- src/main/java/com/enmivida/app/security/security/SecurityConfig.java
+++ src/main/java/com/enmivida/app/security/security/SecurityConfig.java
@@ -4,6 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -23,5 +28,29 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults())
         ;
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+
+        UserDetails admin = User
+                .withUsername("admin")
+                .password("to_be_encoded")
+                .authorities("ADMIN")
+                .build();
+
+        UserDetails user = User
+                .withUsername("user")
+                .password("to_be_encoded")
+                // inserta un array de string
+                .authorities("USER")
+                .build();
+        // inserta un array de userdetails
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
