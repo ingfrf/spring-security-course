459c62ed0cb8332576958dc2a5e6ca4c559f3bb8
ingfrf <ingfrf@gmail.com>
2024-01-22 23:09:50 +0100

    master database users basics

--- src/main/java/com/enmivida/app/security/security/SecurityConfig.java
+++ src/main/java/com/enmivida/app/security/security/SecurityConfig.java
@@ -4,12 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
@@ -31,6 +32,17 @@ public class SecurityConfig {
     }
 
     @Bean
+    public UserDetailsService  userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+
+ /*   @Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
 
         UserDetails admin = User
@@ -52,5 +64,5 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
-    }
+    }*/
 }
