1a369fd2563682edd28718e325ec4fd8315ba6f7
ingfrf <ingfrf@gmail.com>
2024-01-29 23:37:38 +0100

    master cors and csrf

--- src/main/java/com/enmivida/app/security/security/SecurityConfig.java
+++ src/main/java/com/enmivida/app/security/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.enmivida.app.security.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -7,13 +8,30 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
     // Antes de la version 6 era necesario extender de WebSecurityAdapter
 
+    private final CsrfCookieFilter csrfCookieFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // The CsrfTokenRequestAttributeHandler makes the CsrfToken available as an HttpServletRequest attribute called _csrf.
+        // This implementation also resolves the token value from the request as either a request header
+        // (one of X-CSRF-TOKEN or X-XSRF-TOKEN by default) or a request parameter (_csrf by default).
+        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+        requestHandler.setCsrfRequestAttributeName("_csrf");
+
         http
                 .authorizeHttpRequests(auth ->
                         //auth.anyRequest().authenticated()
@@ -24,6 +42,17 @@ public class SecurityConfig {
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults())
         ;
+        http.cors(cors -> corsConfigurationSource());
+        http.csrf(csrf -> csrf
+                        // se añade el handler ya definido
+                        .csrfTokenRequestHandler(requestHandler)
+                        // se excluyen las rutas 'abiertas' que no necesitan autenticacion
+                        .ignoringRequestMatchers("/welcome", "/about_us")
+                        // The CookieCsrfTokenRepository writes to a cookie named XSRF-TOKEN
+                        // and reads it from an HTTP request header named X-XSRF-TOKEN or the request parameter _csrf by default
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                // se añade el filtro definido
+                .addFilterAfter(csrfCookieFilter, BasicAuthenticationFilter.class);
         return http.build();
     }
 
@@ -32,4 +61,24 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        // se puede hacer indicando los dominios exactos
+        // config.setAllowedOrigins(List.of("http://localhost:4200", "http://my-app.com"));
+        // permite cualquier origen
+        config.setAllowedOrigins(List.of("*"));
+
+        // se pueden habilitar solo algunos metodos
+        // config.setAllowedMethods(List.of("GET", "POST"));
+        // o a todos
+        config.setAllowedMethods(List.of("*"));
+        // se admiten todas las cabeceras
+        config.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        // se aplica a todos las rutas
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
 }
